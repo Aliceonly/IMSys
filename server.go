0dcfe5297f28ed5f4a7804edf1ff16255ddc2ba2
Aliceonly <aliceonly@foxmail.com>
2023-11-25 16:43:13 +0800

    feat: client online channel and broadcast

--- server.go
+++ server.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip string
 	Port int
+
+	OnlineMap map[string]*User
+	mapLock sync.RWMutex
+
+	Message chan string
 }
 
 // NewServer creates a new server instance
@@ -15,12 +21,39 @@ func NewServer(ip string, port int) *Server {
 	return &Server{
 		Ip: ip,
 		Port: port,
+		OnlineMap: make(map[string]*User),
+		Message: make(chan string),
 	}
 }
 
+func (this *Server) ListenMessage()  {
+	for {
+		msg := <-this.Message
+
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string)  {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn)  {
-	// logic
-	fmt.Println("connect success")
+	user := NewUser(conn)
+	// add online map
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// broadcast
+	this.BroadCast(user, "now online")
+
+	select {}
 }
 
 // Start starts the server
@@ -33,6 +66,8 @@ func (this *Server) Start()  {
 	}
 	// close listen socket
 	defer listener.Close()
+
+	go this.ListenMessage()
 
 	for {
 		// accept
